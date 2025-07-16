1b3fcb13e885f30e83f79dca777a36943cacebaf
Menna <mennamohali62@gmail.com>
2025-07-16 22:31:06 +0300

    add governorate & blood types tables

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
